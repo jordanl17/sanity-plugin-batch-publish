f8f8401c85dc0c1c12c5f000800392d467ca0e09
Jordan Lawrence <jordanl17@me.com>
2026-06-28 14:48:15 +0100

    feat(02-01): re-export cartSet ops and evaluateCartMembership from public index

--- packages/sanity-plugin-batch-publish/src/index.ts
+++ packages/sanity-plugin-batch-publish/src/index.ts
@@ -2,6 +2,9 @@ import {definePlugin} from 'sanity'
 
 import type {BatchPublishPluginConfig} from './types'
 
+export {addItem, hasItem, removeItem} from './cartSet'
+export {evaluateCartMembership} from './evaluateCartMembership'
+export type {CartMembershipDecision, CartMembershipSnapshot} from './evaluateCartMembership'
 export {isCartCandidate} from './isCartCandidate'
 export type {CartCandidateInput} from './isCartCandidate'
 export type {BatchPublishPluginConfig, CartItem} from './types'
